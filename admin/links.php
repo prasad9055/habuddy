dc951a58b3b69fd7b4a0f971e5612b7e44ba9ab0
prasad9055 <101623934+prasad9055@users.noreply.github.com>
2024-05-17 21:14:51 +0530

    dev slug added

--- admin/links.php
+++ admin/links.php
@@ -2,7 +2,7 @@
 <?php 
    $path = $_SERVER['DOCUMENT_ROOT'];
    $link= $path."/admin";
-  $css_link="https://haabuddy.com/admin";
+  $css_link="https://haabuddy.com/dev/admin";
   
   //local links
  //  $link= $path."/haabuddy/admin";
