bd023c657b81269cc5de8a10b3b9e515bc14671e
prasad9055 <101623934+prasad9055@users.noreply.github.com>
2024-05-29 11:35:07 +0530

    Production url added

--- includes/function.php
+++ includes/function.php
@@ -1,12 +1,20 @@
 <?php 
 
 function base_url($part_url){
-   echo 'http://localhost/habuddy/'.$part_url;
+   //  echo 'http://localhost/habuddy/'.$part_url;
+
+  //  echo 'https://haabuddy.com/'.$part_url;
+   
+    echo 'https://haabuddy.com/dev/'.$part_url;
 }
 
 function base_url_return($part_url){
 
-   return 'http://localhost/habuddy/'.$part_url;
+//   return 'http://localhost/habuddy/'.$part_url;
+
+//    return 'https://haabuddy.com/'.$part_url;
+
+    return 'https://haabuddy.com/dev/'.$part_url;
 }
 
 function include_link($url){
